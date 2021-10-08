f087dafbc403b8b9d9f7ffbc6ac02fb01c73356b
micah66 <micahgordon66@gmail.com>
2021-10-08 11:16:12 +0300

    add vocabulary words model with mock data

---
+++ src/app/mocks/vocab-words.ts
@@ -0,0 +1,6 @@
+import { VocabularyWord } from "../models/vocabulary-word";
+
+export const VOCAB_WORDS: VocabularyWord[] =[
+  { inEnglish: 'yes', inHebrew: 'כן' },
+  { inEnglish: 'no', inHebrew: 'לא' }
+]
